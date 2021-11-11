9934f64f43b02cab9c82064c7b860473dfb5bdb4
Ramakishore Nooji <rnooji36@in.ibm.com>
2021-11-11 10:43:22 +0530

    made some code change in flask app

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 application = Flask(__name__)
 
 @application.route('/')
-def hello_world():
-	return 'This is a dev instance with the code verision 1.1, added via AWS pipeline for CI/CD activities'
-	
+def hello():
+    return 'Web App with Python Flask!'
+
+if __name__ == '__main__':
+	application.run(host='', port=5000)
